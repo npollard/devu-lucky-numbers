2c6f47a896e55f2820254764798d67e27a734ac6
Nelson Pollard <extern.nelson.pollard@vw.com>
2015-11-11 15:05:36 -0800

    init

---
+++ Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(10, 4));
+
+    }
+}
